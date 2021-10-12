7ce3411ccbffebeb9f8f09eaf5045b2c1eb47560
YOUSEF ALMALKI <mmrdas600@gmail.com>
2021-10-12 14:30:35 +0300

    My game

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var dice = 6
+
+if dice == 1 {
+    print("1")
+    
+}else if  dice == 2 {
+    
+    print("2")
+    
+}else if  dice == 3 {
+    
+    print("3")
+                
+                
+}else if  dice == 4 {
+    
+    print("4")
+    
+}else if  dice == 5 {
+    
+    print("5")
+}else if  dice == 6 {
+    
+    print("6")
+}else {
+    print("nothing ")
+}
+
+
+
+
+let dice1 = "snake"
+switch dice1 {
+    
+case "snake":print("move down")
+case  "lagger": print("move up")
+
+    
+default:
+    print("nothing")
+}
+
