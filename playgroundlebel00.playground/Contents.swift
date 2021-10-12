b30991c2ada106941144d751fc96ff9966ca857b
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-10-12 12:08:18 +0300

    lab3

---
+++ playgroundlebel00.playground/Contents.swift
@@ -0,0 +1,26 @@
+// if statement
+var dicesResult = 6
+if (dicesResult == 1){
+    print("The Dice result is 1")
+} else if (dicesResult == 2) {
+    print("The Dice result is 2")
+} else if (dicesResult == 3) {
+    print("The Dice result is 3")
+} else if (dicesResult == 4) {
+    print("The Dice result is 4")
+}  else if (dicesResult == 5) {
+    print("The Dice result is 5")
+} else if (dicesResult == 6) {
+    print("The Dice result is 6")
+}
+
+//switch
+var squar = "Snake"
+switch squar {
+case "Lagger":
+    print("Go Up")
+    case "Snake":
+    print("Go Down")
+default:
+    print("nothing")
+}
