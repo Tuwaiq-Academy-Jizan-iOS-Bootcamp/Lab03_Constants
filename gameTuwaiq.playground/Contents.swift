c177a2ea4289228bc9e9a0806e06ba4ef09bb602
dahma alwani <dahmaalwani@MacBook-Pro-dahma.local>
2021-10-12 13:32:42 -0700

    lab 3

---
+++ gameTuwaiq.playground/Contents.swift
@@ -0,0 +1,26 @@
+var dice = 6
+if (dice == 1){
+    print(dice, 1)
+}else if (dice == 2){
+        print("dice, 2")
+}else if (dice == 3){
+    print("dice, 3")
+}else if (dice == 4){
+    print("dice, 4")
+}else if (dice == 5){
+    print("dice, 5")
+}else if (dice == 6){
+    print("dice, 6")
+}else {
+    print("number of String")
+}
+var loc = "laggar"
+switch loc{
+case "laggar":
+    print("move up")
+case "snake":
+    print("move down")
+default:
+    print("thair nothing")
+}
+        
