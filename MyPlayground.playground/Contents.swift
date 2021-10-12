880bea2d3d1d46c9dd3cbeb15401fa2d49466b80
Hanan Somily <hanansomily@MacBook-Air-Hanan.local>
2021-10-12 11:50:13 +0300

    my 3lab

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+var diceNumber = 4
+var action = "lagger"
+
+
+if diceNumber == 1{
+    
+    print("1")
+}
+else if diceNumber == 2{
+    print("2")
+}
+
+else if diceNumber == 3 {
+    print("3")
+}
+else if diceNumber == 4{
+    print("4")
+}
+else if diceNumber == 5{
+    print("5")
+}
+else if diceNumber == 6{
+    print("6")
+}
+
+switch action {
+case "lagger" :print ("move up")
+case "snak":print("move dawen")
+default :print("nothing")
+    
+}
+
+
+
+
+
+
+
+
