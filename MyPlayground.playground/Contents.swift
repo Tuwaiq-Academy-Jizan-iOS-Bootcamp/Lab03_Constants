66abc1b1dc75636ab1bdca499e861ac207d2dc0f
Dua'a ageel <duaaageel@MacBook-Pro-Duaa.local>
2021-10-12 12:23:21 +0300

    3lap

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+var diceFace = 4
+if(diceFace==1){
+    print("move 1 step")
+        }else if(diceFace==2){
+    print("move 2 step")
+        }else if(diceFace==3){
+            print("move 3 step")
+
+        }else if(diceFace==4){
+            print("move 4 step")
+
+        }else if(diceFace==5){
+            print("move 5 step")
+
+        }else if(diceFace==6){
+            print("move 6 step")
+                            
+
+        }else{
+            print("the dice not available")
+
+        }
+var myStep = "snake"
+switch (myStep){
+case "lagger":
+    print("move up")
+case "snake":
+    print(" move dawon")
+default: print("no thing")
+}
+                                 
