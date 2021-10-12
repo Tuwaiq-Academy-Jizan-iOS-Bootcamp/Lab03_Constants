6e16640324ca6669a7f3395ebfa53f9a6e7371e5
MUSAMASMALI <musa14077@yahoo.com>
2021-10-12 14:35:01 +0300

    done

---
+++ lab 03 conditionals.playground/Contents.swift
@@ -0,0 +1,33 @@
+var dice = 6
+if dice == 1{
+    print (1)
+}
+if dice == 2{
+    print(2)
+}
+if dice == 3 {
+    print (3)
+}
+if dice == 4{
+    print (4)
+}
+if dice == 5{
+    print (5)
+}
+if dice == 6{
+    print (6)
+}
+// switch
+var squar = "snake"
+switch squar{
+case "lagger":
+    print("Go Up")
+case "snake":
+    print("Go Down")
+default:
+    print("nothing")
+    
+}
+    
+     
+    
