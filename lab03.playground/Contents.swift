e7e0431d80ba47e2a983ced6347420135ef1cc4f
Jawaher Mohammad <jawaher.@MacBook-Air-Jawaher.local>
2021-10-12 12:09:08 +0300

    My Lagger & Snake Game

---
+++ lab03.playground/Contents.swift
@@ -0,0 +1,25 @@
+var diceResult=5
+if (diceResult==1){
+    print("The Dice's result is 1")
+} else if (diceResult==2){
+    print("The Dice's result is 2")
+}else if (diceResult==3){
+    print("The Dice's result is 3")
+}else if  (diceResult==4){
+    print("The Dice's result is 4")
+}else if  (diceResult==5){
+    print("The Dice's result is 5")
+}else{
+    print("The Dice's result is 6")
+}
+
+var square="Lagger"
+
+switch square{
+case "Lagger":
+    print("Go up")
+case "Snake":
+    print("Go down")
+default:
+    print("nothing")
+}
