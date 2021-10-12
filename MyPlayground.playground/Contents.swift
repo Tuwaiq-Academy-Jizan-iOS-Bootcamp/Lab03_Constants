539af401d980bbd395061a49d4b4a2db6685c053
زهور حسين <zhoorhussain@MacBook-Air-zhwr.local>
2021-10-12 15:51:10 +0300

    lab zhoor

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,6 @@
 
 var dicNumber = 5
-var action = "snak"
+var action = "snake"
 
 if dicNumber == 1 {
     print("1")
@@ -20,8 +20,61 @@ else if dicNumber == 4 {
 else if dicNumber == 6 {
     print("6")
 }
-switch action {
-case "snak" : print("move dawon")
-case "leqqer" : print("move up")
+switch "action"
+{
+case "snake" : print("move dawon")
+case "Lagger" : print("move up")
 default : print("nothing")
+}
+
+let squareState2 = ""
+let squareNumber = 5
+let board = 25
+
+
+if (squareNumber > board) {
+    print("You are out of the board")
+}else {
+    print("You are in the Game")
+}
+
+switch squareState2 {
+    
+case "Lagger":
+    if (squareNumber == 3) || (squareNumber == 6) || (squareNumber == 9) || (squareNumber == 10) {
+        if squareNumber == 3 {
+            print("Move to 11")
+        }
+        if squareNumber == 6 {
+            print("Move to 17")
+        }
+        if squareNumber == 9 {
+            print("Move to 18")
+        }
+        if squareNumber == 10 {
+            print("Move to 12")
+        }
+    } else {
+        print("Nothing to do !")
+    }
+case "Snake" :
+    if (squareNumber == 14) || (squareNumber == 19) || (squareNumber == 22) || (squareNumber == 24) {
+        if (squareNumber == 14) {
+            print("Move to 4")
+        }
+        if (squareNumber == 19) {
+            print("Move to 8")
+        }
+        if (squareNumber == 22) {
+            print("Move to 20")
+        }
+        if (squareNumber == 24) {
+            print("Move to 16")
+        }
+    } else {
+        print("Nothing to do !")
+    }
+    
+default: print("Nothing to do !")
+    
 }
