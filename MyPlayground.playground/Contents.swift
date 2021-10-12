9f198cca40929bbcbf45df0f71c538065f095983
زهور حسين <zhoorhussain@MacBook-Air-zhwr.local>
2021-10-12 13:05:50 +0300

    lap3 zhoor

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+
+var dicNumber = 5
+var action = "snak"
+
+if dicNumber == 1 {
+    print("1")
+}
+else if dicNumber == 2 {
+    print("2")
+}
+else if dicNumber == 3 {
+    print("3")
+}
+else if dicNumber == 4 {
+    print("4")
+}
+    else if dicNumber == 5 {
+        print("5")
+    }
+else if dicNumber == 6 {
+    print("6")
+}
+switch action {
+case "snak" : print("move dawon")
+case "leqqer" : print("move up")
+default : print("nothing")
+}
