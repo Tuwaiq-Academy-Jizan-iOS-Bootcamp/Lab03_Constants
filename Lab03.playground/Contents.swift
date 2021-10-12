b55e41f14d0649bf904113c0a72f9239a345ca78
Afrah Omar <afrahomar@MacBook-Pro-Afrah.local>
2021-10-12 13:10:47 +0300

    lab-3

---
+++ Lab03.playground/Contents.swift
@@ -0,0 +1,30 @@
+var dic = 6
+if (dic == 1) {
+    print ("dice 1")
+}
+else if (dic == 2){
+    print ("dice 2")
+}
+else if (dic == 3){
+    print ("dice 3")
+}
+else if (dic == 4){
+    print ("dice 4")
+}
+else if (dic == 5){
+    print ("dice 5")
+}
+else if (dic == 6){
+    print ("dice 6")
+}
+
+var loc = "Lagger"
+switch loc{
+case "Lagger":
+    print("move up")
+case "Snake":
+    print("move down")
+default:
+    print("thair nothing")
+}
+
