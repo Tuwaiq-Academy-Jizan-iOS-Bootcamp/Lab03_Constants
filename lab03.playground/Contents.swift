749eaaf1f01a21241799be293f39222f552595c7
Bushra Barakat <bushrabarakat@MacBook-Pro-Bushra.local>
2021-10-12 12:09:16 +0300

    add lab03

---
+++ lab03.playground/Contents.swift
@@ -0,0 +1,23 @@
+var dicesResult = 6
+
+if(dicesResult == 1) {
+    print(" face 1 ")
+}else if (dicesResult == 2) {
+    print(" face 2 ")
+}else if (dicesResult == 3) {
+    print("face 3 ")
+}else if(dicesResult == 4) {
+    print ("face 4")
+}else if( dicesResult == 5 ) {
+    print (" face 5 ")
+}else {
+    print ("face 6")
+}
+
+
+var action = "snake"
+switch action{
+case "snake" : print ("go up")
+case "lagger" : print ("go down")
+    default : print ("nothing")
+}
