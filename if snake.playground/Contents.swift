1c21d1e380284ed6bbe10ff71d41c03841f48bdf
Ehab hakami <ehab7k@gmail.com>
2021-10-12 12:21:01 +0300

    ehab hakamilab3

---
+++ if snake.playground/Contents.swift
@@ -0,0 +1,39 @@
+let myNumber = 4
+
+if (myNumber <= 1) {
+    print("1")
+}
+
+else if (myNumber <= 2){
+    print("2")
+}
+
+else if (myNumber <= 3){
+    print("3")
+}
+
+else if (myNumber <= 4){
+    print("4")
+}
+
+else if (myNumber <= 5){
+    print("5")
+}
+else if (myNumber <= 6){
+    print("6")
+}
+    else {
+    print("nymber lock")
+    }
+let action = "snake"
+
+switch action {
+    
+case "snake": print("move down")
+case "ledger": print("move up")
+
+    
+    
+default: action
+    print("invalid dice")
+}
