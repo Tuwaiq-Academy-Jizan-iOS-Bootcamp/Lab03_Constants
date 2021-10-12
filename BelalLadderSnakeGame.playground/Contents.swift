06d9218290a113d7a8eeea9cc50d6ea93388de21
Belal <mr_belal@Belals-MacBook-Air.local>
2021-10-12 13:39:07 +0300

    BelalFirstGame

---
+++ BelalLadderSnakeGame.playground/Contents.swift
@@ -0,0 +1,67 @@
+var dice = 5
+var squareLocation = 6
+let ladder = "You're on ladder move up to square number"
+let snake = "You're on snake move back square number"
+let safe = "You're safe"
+var player = dice + squareLocation
+if player == 1 {
+    print(safe)
+}else if player == 2 {
+    print(safe)
+}else if player == 3 {
+    print("\(ladder) 11")
+}else if player == 4 {
+    print(safe)
+}else if player == 5 {
+    print(safe)
+}else if player == 6 {
+    print("\(ladder) 17")
+}else if player == 7 {
+    print(safe)
+}else if player == 8 {
+    print(safe)
+}else if player == 9 {
+    print("\(ladder) 18")
+}else if player == 10 {
+    print("\(ladder) 12")
+}else if player == 11 {
+    print(safe)
+}else if player == 12 {
+    print(safe)
+}else if player == 13 {
+    print(safe)
+}else if player == 14 {
+    print("\(snake) 4")
+}else if player == 15 {
+    print(safe)
+}else if player == 16 {
+    print(safe)
+}else if player == 17 {
+    print(safe)
+}else if player == 18 {
+    print(safe)
+}else if player == 19 {
+    print(safe)
+}else if player == 15 {
+    print(safe)
+}else if player == 16 {
+    print(safe)
+}else if player == 17 {
+    print(safe)
+}else if player == 18 {
+    print(safe)
+}else if player == 19 {
+    print("\(snake) 8")
+}else if player == 20 {
+    print(safe)
+}else if player == 21 {
+    print(safe)
+}else if player == 22 {
+    print("\(snake) 20")
+}else if player == 23 {
+    print(safe)
+}else if player == 24 {
+    print("\(snake) 16")
+}else if player == 25 {
+    print(safe)
+}
